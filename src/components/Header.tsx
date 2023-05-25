2b26cacd73dcbe8d56925f5da71ede8b0bd0674b
hyungjun-tech01 <whmoon00@naver.com>
2023-05-25 21:43:23 +0900

    header 작업중

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from '../logo.svg';
 import styled from "styled-components";
+
 const Logo = styled.img.attrs({
     src : `${logo}`
 })`
     width : 50px;
 `;
+const Items = styled.div``;
+const Item = styled.div``;
 function Header(){
     return (
         <Logo />
