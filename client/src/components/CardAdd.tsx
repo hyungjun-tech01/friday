3d5b790b3685ffcca4434fae9ef939a0acb1b375
hyungjun-tech01 <whmoon00@naver.com>
2023-08-10 21:35:39 +0900

    카드 add 작성중

---
+++ client/src/components/CardAdd.tsx
@@ -0,0 +1,58 @@
+import {useEffect, useRef} from "react";
+import styles from "../scss/CardAdd.module.scss";
+import { Button, Form, TextArea } from 'semantic-ui-react';
+
+interface ICardAddProps{
+    setIsCardAddOpened:(value:boolean) => void;
+}
+function CardAdd({setIsCardAddOpened}:ICardAddProps){
+    let wrapperRef = useRef<any>(null); //모달창 가장 바깥쪽 태그를 감싸주는 역할
+
+    useEffect(()=>{
+      document.addEventListener('mousedown', handleClickOutside);
+      return()=>{
+        document.removeEventListener('mousedown', handleClickOutside);
+      }
+    });
+
+    const handleClickOutside=(event:any)=>{
+      if (wrapperRef && 
+          wrapperRef.current &&
+          !wrapperRef.current.contains(event.target)) {
+        console.log('close modal');
+        setIsCardAddOpened(false);
+      }
+    }        
+    return (
+        <div ref={wrapperRef}>
+            <Form>
+        <div className={styles.fieldWrapper}>
+            <TextArea
+            ref={nameField}
+            as={TextareaAutosize}
+            name="name"
+            value={data.name}
+            placeholder={t('common.enterCardTitle')}
+            minRows={3}
+            spellCheck={false}
+            className={styles.field}
+            onKeyDown={handleFieldKeyDown}
+            onChange={handleFieldChange}
+            onBlur={handleFieldBlur}
+            />
+      </div>
+      <div className={styles.controls}>
+        {/* eslint-disable-next-line jsx-a11y/mouse-events-have-key-events */}
+        <Button
+          content={t('action.addCard')}
+          className={styles.submitButton}
+          onMouseOver={handleControlMouseOver}
+          onMouseOut={handleControlMouseOut}
+        />
+      </div>
+      </Form>
+        </div>
+    );
+}
+
+export default CardAdd;
--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -6,59 +6,61 @@ import {useState, useEffect} from "react";
 import Card from "./Card";
 import { ReactComponent as PlusMathIcon } from '../image/plus-math-icon.svg';
 import {useTranslation} from "react-i18next";
-
+import CardAdd from "./CardAdd";
 interface IListProps{
     id:string;
     index:number;
     name : string;
 }
-    function List({id, index, name}:IListProps){
-        const [t] = useTranslation();
-    //    const selectCards = useRecoilValue(cardSelector); // 호출 가능한 함수를 가져옴
-    //    console.log('isCardLoading',isCardLoading);
-    //    if(!isCardLoading) 
-    //        selectedCards = selectCards(id);
-    //    console.log(id, selectedCards);
-        
-        const [isCardLoading, setIsCardLoading] = useState(true);
-        const [cards, setCards] = useState<ICard[]>();
-        const onQueryCards = async (id:string) => {
-            const response = await apiGetCardsbyListId(id);    
-            if(!response.message){
-                console.log('카드조회', response);  
-                setCards(response);
-                setIsCardLoading(false);
-                console.log(cards);
-            }
+function List({id, index, name}:IListProps){
+    const [t] = useTranslation();
+//    const selectCards = useRecoilValue(cardSelector); // 호출 가능한 함수를 가져옴
+//    console.log('isCardLoading',isCardLoading);
+//    if(!isCardLoading) 
+//        selectedCards = selectCards(id);
+//    console.log(id, selectedCards);
+    
+    const [isCardLoading, setIsCardLoading] = useState(true);
+    const [cards, setCards] = useState<ICard[]>();
+    const onQueryCards = async (id:string) => {
+        const response = await apiGetCardsbyListId(id);    
+        if(!response.message){
+            console.log('카드조회', response);  
+            setCards(response);
+            setIsCardLoading(false);
+            console.log(cards);
         }
-        useEffect(
-            () => { onQueryCards(id); } ,[id]
-        );
-        const handleAddCardClick = () => {
-            console.log('addcard');
-        }
-        return(
-            <div className={styles.innerWrapper}>
-            <div className={styles.outerWrapper}>
-                <div className={styles.header}>
-                    <div className={styles.headerName}>{name}</div>
-                    <div className={styles.cards}>
-                        {!isCardLoading&&cards?.map((card)=>(<Card key={card.cardId} card={card}/>))}
-                        <button
-                            type="button"
-                            className={styles.addCardButton}
-                            onClick={handleAddCardClick}
-                        >
-                  <PlusMathIcon className={styles.addCardButtonIcon} />
-                  <span className={styles.addCardButtonText}>
-                    {cards  && ( t('action.addAnotherCard') )} 
-                    {!cards && ( t('action.addCard'))}
-                  </span>
-                </button>
-                    </div>
+    }
+    useEffect(
+        () => { onQueryCards(id); } ,[id]
+    );
+    const handleAddCardClick = () => {
+        console.log('addcard');
+    }
+    const [isCardAddOpened, setIsCardAddOpened] = useState(false);
+    return(
+        <div className={styles.innerWrapper}>
+        <div className={styles.outerWrapper}>
+            <div className={styles.header}>
+                <div className={styles.headerName}>{name}</div>
+                <div className={styles.cards}>
+                    {!isCardLoading&&cards?.map((card)=>(<Card key={card.cardId} card={card}/>))}
+                    {isCardAddOpened&&<CardAdd setIsCardAddOpened={setIsCardAddOpened}/>}
+                    <button
+                        type="button"
+                        className={styles.addCardButton}
+                        onClick={handleAddCardClick}
+                    >
+                        <PlusMathIcon className={styles.addCardButtonIcon} />
+                        <span className={styles.addCardButtonText}>
+                            {cards  && ( t('action.addAnotherCard') )} 
+                            {!cards && ( t('action.addCard'))}
+                        </span>
+                    </button>
                 </div>
             </div>
-            </div>
-        );
-    }
-    export default List ; 
+        </div>
+        </div>
+    );
+}
+export default List ; 
