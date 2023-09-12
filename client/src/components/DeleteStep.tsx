8c94aaf0cd7e725e1357b9787306ab57e70240e6
hyungjun <hyungjun@sindoh.com>
2023-09-12 17:46:05 +0900

    보드에서 나가기 작업중

--- client/src/components/DeleteStep.tsx
+++ client/src/components/DeleteStep.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'semantic-ui-react';
+import { Button , Popup as SemanticUIPopup} from 'semantic-ui-react';
 
 
 import styles from '../scss/DeleteStep.module.scss';
@@ -12,17 +12,42 @@ interface IDeleteStep{
     onConfirm : ()=>void;
     onBack : () => void;
 }
+const handleMouseEnter = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.currentTarget.style.background = '#9e3f08';
+    e.currentTarget.style.color = 'white';
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.currentTarget.style.background = '#db570a';
+    e.currentTarget.style.color = 'white';
+  };
+  
 function DeleteStep ({ title, content, buttonContent, onConfirm, onBack }:IDeleteStep) {
     return (
-        <>
-          <div onClick={onBack}>
-            {title}
-          </div>
-          <div>
-            <div className={styles.content}>{content}</div>
-            <Button fluid negative content={buttonContent} onClick={onConfirm} />
-          </div>
-        </>
+        <div className={styles.overlay} > 
+            <div className={styles.modal} >
+            <SemanticUIPopup.Header className={styles.wrapper}>
+                {onBack && <Button icon="angle left" onClick={onBack} className={styles.backButton} />}
+                <div className={styles.popupcontent}>{title}</div>
+            </SemanticUIPopup.Header>
+            <div>
+                    <div className={styles.content}>{content}</div>
+                    <Button fluid positive 
+                     style = {{
+                        background : '#db570a',
+                        color: 'white',
+                        fontWeight: 'bold',
+                        marginTop: '8px',
+                        padding: '6px 11px',
+                        textAlign: 'center',
+                        transition: 'none',
+                      }} 
+                      onMouseEnter={handleMouseEnter}
+                      onMouseLeave={handleMouseLeave}                      
+                    content={buttonContent} onClick={onConfirm} />
+              </div>
+            </div>  
+        </div>
       );
 }
 
--- server/server.js
+++ server/server.js
@@ -451,7 +451,7 @@ app.post('/modifyCard', async(req, res) => {
         cardStatusActionType ,
         cardStatusId } = req.body;
     try{
-        console.log('modify board');
+        console.log('modify card');
         // insert project 
         const response = await pool.query(`call p_modify_card($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, 
                                            $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36)`,
