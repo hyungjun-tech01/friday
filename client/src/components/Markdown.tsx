42e64fcae8a5c68ace03e1d99fed687c3e861670
hyungjun-tech01 <whmoon00@naver.com>
2023-10-30 20:46:49 +0900

    eslint 여러줄 -> 1줄로 변경

--- client/src/components/Markdown.tsx
+++ client/src/components/Markdown.tsx
@@ -20,9 +20,7 @@ const Markdown = ({linkStopPropagation, ...props}:IMarkdownProps) => {
 
   const linkRenderer = useCallback(
     ({ ...linkProps }) => (
-      /* eslint-disable-next-line jsx-a11y/anchor-has-content,
-                                  jsx-a11y/click-events-have-key-events,
-                                  jsx-a11y/no-static-element-interactions */
+      /* eslint-disable-next-line jsx-a11y/anchor-has-content, jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
       <a
         {...linkProps} // eslint-disable-line react/jsx-props-no-spreading
         rel={
